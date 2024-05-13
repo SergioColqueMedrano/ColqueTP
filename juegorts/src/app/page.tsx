a11424b62202e4ab6f53063b4ad45cf08b1b4924
“tyraiel” <tyraiel@gmail.com>
2024-05-13 09:12:59 -0300

    mapa

--- juegorts/src/app/page.tsx
+++ juegorts/src/app/page.tsx
@@ -1,6 +1,12 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <h1>Home Page</h1>
+    <div className="bg-map h-screen w-screen bg-cover">
+    </div>
+    
+    </>
   )
 }
--- juegorts/tailwind.config.ts
+++ juegorts/tailwind.config.ts
@@ -9,6 +9,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
+        "map": "url(/mapa.png)",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
